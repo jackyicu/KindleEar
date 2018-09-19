9775297239004dba38609d9c07ac9f97bb4aebf2
jackyicu <jackyicu@gmail.com>
2018-09-19 22:04:17 +0800

    revise book rss and config.py

--- books/PingWestCN.py
+++ books/PingWestCN.py
@@ -11,18 +11,14 @@ class PingWestCN(BaseFeedBook):
     language              = 'zh-cn'
     feed_encoding         = "utf-8"
     page_encoding         = "utf-8"
-    deliver_times         = [6,17] #6:00,17:00两次推送
-    oldest_article        = 39600 #11*60*60
+    deliver_times         = [5,17] #5:00,17:00两次推送
+    oldest_article        = 43200 #12*60*60
     mastheadfile          = "mh_pingwestcn.gif"
     coverfile             = "cv_pingwestcn.jpg"
     network_timeout       = 60
-    fetch_img_via_ssl     = False
+
     feeds = [
-            (u'PingWest中文网', 'http://www.pentitugua.com/rss.xml', True),
+            (u'PingWest中文网', 'https://www.pingwest.com/feed', True),
            ]
     
-    # def soupbeforeimage(self, soup):
-    #     # 更换另一个图库，因为RSS中的图库已经被封
-    #     for img in soup.find_all('img', attrs={'src':True}):
-    #         if img['src'].startswith('http://ptimg.org:88'):
-    #             img['src'] = img['src'].replace('http://ptimg.org:88','http://pic.yupoo.com')
+ 
